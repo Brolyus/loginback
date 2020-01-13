66b0968228510ce22bccf865b6e2423aedb66600
Brolyus <desousanthony@gmail.com>
2020-01-13 16:12:00 +0100

    post route work

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const port = 8080
 const app = express()
 const cors = require('cors')
 
+const userData = {
+    email: 'amusant@test.com',
+    password: 'azerty'
+}
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -15,6 +20,15 @@ app.get('/api/home', function(req, res) {
 })
 app.get('/api/secret', function(req, res) {
     res.send('The password is potato')
+})
+
+app.post('/api/home', function(req, res) {
+    const formData = req.body
+    if (userData.password !== formData.password) {
+        res.send('Mauvais password')
+    } else {
+        res.send('ok')
+    }
 })
 
 app.listen(port, err => {
