4eafdb7ba2bf3511f171bee0840a39977c5aa81d
Brolyus <desousanthony@gmail.com>
2020-01-13 15:26:06 +0100

    sharing

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const connection = require('./config')
+const port = 8080
+const app = express()
+const cors = require('cors')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+app.get('/api/home', function(req, res) {
+    res.send('Welcome!')
+})
+app.get('/api/secret', function(req, res) {
+    res.send('The password is potato')
+})
+
+app.listen(port, err => {
+    if (err) {
+        throw new Error('Something bad happened...')
+    }
+
+    console.log(`Server is listening on ${port}`)
+})
